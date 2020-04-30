b45ce021ac45dfd3d221c2d031e7da1da60883d6
mrkvmtys <40655916+mrkvmtys@users.noreply.github.com>
2020-04-30 19:40:09 +0200

    Add files via upload
    Replace unnecessary parts from original .tcx files

---
+++ tcx_transformation.ps1
@@ -0,0 +1,12 @@
+﻿# remove unnecessary parts from the TCX files
+# otherwise they are not processable in R
+cd C:\Users\mrm\Desktop\strava\activities
+
+$configFiles = Get-ChildItem . *.tcx -rec
+
+foreach ($file in $configFiles)
+{
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace [Regex]::Escape('<?xml version="1.0" encoding="UTF-8"?>'), "" } |
+    Set-Content $file.PSPath
+}
