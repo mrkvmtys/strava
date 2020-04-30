cef847b6f8263fac92ba2614956bf55f5edbc6d9
mrkvmtys <40655916+mrkvmtys@users.noreply.github.com>
2020-04-30 19:49:16 +0200

    Update tcx_transformation.ps1

--- tcx_transformation.ps1
+++ tcx_transformation.ps1
@@ -1,9 +1,12 @@
 ﻿# remove unnecessary parts from the TCX files
 # otherwise they are not processable in R
-cd C:\Users\mrm\Desktop\strava\activities
+# navigate to the "activities" directory where you unzipped the strava bulk export 
+cd C:\Users\...\strava\activities
 
+# get list of files for looping
 $configFiles = Get-ChildItem . *.tcx -rec
 
+# remove unnecessary header
 foreach ($file in $configFiles)
 {
     (Get-Content $file.PSPath) |
